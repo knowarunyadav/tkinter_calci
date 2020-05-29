b67c1732a69737fad046d93a3a2abce5d7c086c3
knowarunyadav <35633040+knowarunyadav@users.noreply.github.com>
2020-05-29 14:10:49 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+
+from cx_Freeze import setup,Executable
+
+setup(name='hello',
+      version='0.1',
+      description='normal calci',
+      executables = [Executable(r"C:\Users\khushboo\.spyder-py3\calci\Calci.py")]
+      
+      )
